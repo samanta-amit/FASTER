15baa3f2618bdd4ad2bbec96c026c0efb64f45df
Amit Samanta <amit.samanta049@gmail.com>
2020-07-10 07:45:36 +0530

    fixed minor error

--- cc/src/environment/file_linux.cc
+++ cc/src/environment/file_linux.cc
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 #include <cstring>
-#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <linux/fs.h>
 #include <errno.h>
@@ -11,8 +10,6 @@
 #include <stdio.h>
 #include <time.h>
 #include "file_linux.h"
-
-static int fd;
 
 namespace FASTER {
 namespace environment {
@@ -27,7 +24,6 @@ do { \
 #else
 #define DCHECK_ALIGNMENT(o, l, b) do {} while(0)
 #endif
-	
 
 Status File::Open(int flags, FileCreateDisposition create_disposition, bool* exists) {
   if(exists) {
@@ -107,6 +103,21 @@ int File::GetCreateDisposition(FileCreateDisposition create_disposition) {
   }
 }
 
+void QueueIoHandler::IoCompletionCallback(io_context_t ctx, struct iocb* iocb, long res,
+    long res2) {
+  auto callback_context = make_context_unique_ptr<IoCallbackContext>(
+                            reinterpret_cast<IoCallbackContext*>(iocb));
+  size_t bytes_transferred;
+  Status return_status;
+  if(res < 0) {
+    return_status = Status::IOError;
+    bytes_transferred = 0;
+  } else {
+    return_status = Status::Ok;
+    bytes_transferred = res;
+  }
+  callback_context->callback(callback_context->caller_context, return_status, bytes_transferred);
+}
 
 /// Added later for IO Uring
 void UringQueueIoHandler::UringIoCompletionCallback(io_context_t ctx, struct iocb* iocb, long res,
@@ -126,6 +137,19 @@ void UringQueueIoHandler::UringIoCompletionCallback(io_context_t ctx, struct ioc
 }
 /// Added later for IO Uring
 
+bool QueueIoHandler::TryComplete() {
+  struct timespec timeout;
+  std::memset(&timeout, 0, sizeof(timeout));
+  struct io_event events[1];
+  int result = ::io_getevents(io_object_, 1, 1, events, &timeout);
+  if(result == 1) {
+    io_callback_t callback = reinterpret_cast<io_callback_t>(events[0].data);
+    callback(io_object_, events[0].obj, events[0].res, events[0].res2);
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /// Added later for IO Uring
 bool UringQueueIoHandler::TryComplete() {
@@ -142,6 +166,68 @@ bool UringQueueIoHandler::TryComplete() {
   }
 }
 /// Added later for IO Uring
+
+
+Status QueueFile::Open(FileCreateDisposition create_disposition, const FileOptions& options,
+                       QueueIoHandler* handler, bool* exists) {
+  int flags = 0;
+  if(options.unbuffered) {
+    flags |= O_DIRECT;
+  }
+  RETURN_NOT_OK(File::Open(flags, create_disposition, exists));
+  if(exists && !*exists) {
+    return Status::Ok;
+  }
+
+  io_object_ = handler->io_object();
+  return Status::Ok;
+}
+
+Status QueueFile::Read(size_t offset, uint32_t length, uint8_t* buffer,
+                       IAsyncContext& context, AsyncIOCallback callback) const {
+  DCHECK_ALIGNMENT(offset, length, buffer);
+#ifdef IO_STATISTICS
+  ++read_count_;
+  bytes_read_ += length;
+#endif
+  return const_cast<QueueFile*>(this)->ScheduleOperation(FileOperationType::Read, buffer,
+         offset, length, context, callback);
+}
+
+Status QueueFile::Write(size_t offset, uint32_t length, const uint8_t* buffer,
+                        IAsyncContext& context, AsyncIOCallback callback) {
+  DCHECK_ALIGNMENT(offset, length, buffer);
+#ifdef IO_STATISTICS
+  bytes_written_ += length;
+#endif
+  return ScheduleOperation(FileOperationType::Write, const_cast<uint8_t*>(buffer), offset, length,
+                           context, callback);
+}
+
+Status QueueFile::ScheduleOperation(FileOperationType operationType, uint8_t* buffer,
+                                    size_t offset, uint32_t length, IAsyncContext& context,
+                                    AsyncIOCallback callback) {
+  auto io_context = alloc_context<QueueIoHandler::IoCallbackContext>(sizeof(
+                      QueueIoHandler::IoCallbackContext));
+  if(!io_context.get()) return Status::OutOfMemory;
+
+  IAsyncContext* caller_context_copy;
+  RETURN_NOT_OK(context.DeepCopy(caller_context_copy));
+
+  new(io_context.get()) QueueIoHandler::IoCallbackContext(operationType, fd_, offset, length,
+      buffer, caller_context_copy, callback);
+
+  struct iocb* iocbs[1];
+  iocbs[0] = reinterpret_cast<struct iocb*>(io_context.get());
+
+  int result = ::io_submit(io_object_, 1, iocbs);
+  if(result != 1) {
+    return Status::IOError;
+  }
+
+  io_context.release();
+  return Status::Ok;
+}
 
 /// Added later for IO Uring
 Status UringQueueFile::Open(FileCreateDisposition create_disposition, const FileOptions& options,
@@ -170,7 +256,6 @@ Status UringQueueFile::Read(size_t offset, uint32_t length, uint8_t* buffer,
          offset, length, context, callback);
 }
 
-	
 Status UringQueueFile::Write(size_t offset, uint32_t length, const uint8_t* buffer,
                         IAsyncContext& context, AsyncIOCallback callback) {
   DCHECK_ALIGNMENT(offset, length, buffer);
